86e3dd667f8bcd85e84571141579c2d928a1501a
Alexander Bjerkan <alb@knowit.no>
2017-05-29 13:16:17 +0200

    Added health check

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.get('/health', (req, res) => {
+      res.status(200).json({ status: 200, text: 'Health check ok' });
+    });
+
+    server.get('*', (req, res) => handle(req, res));
+
+    server.listen(3000, (err) => {
+      if (err) throw err;
+      console.log('> Ready on http://localhost:3000');
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
