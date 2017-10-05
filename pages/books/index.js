22d44f2abe40d1aeead98f6ccd95fee948f6c699
Alexander Bjerkan <alb@knowit.no>
2017-10-05 13:15:05 +0200

    Only send reading-level to API if defined

--- pages/books/index.js
+++ pages/books/index.js
@@ -59,11 +59,15 @@ class BooksPage extends React.Component<Props> {
       fetch(`${env.bookApiUrl}/book-api/v1/editorpicks/${language || ''}`),
       fetch(
         `${env.bookApiUrl}/book-api/v1/books/${language ||
-          ''}?sort=popular&page-size=${BOOKS_PAGE_SIZE}&reading-level=${level}`,
+          ''}?sort=popular&page-size=${BOOKS_PAGE_SIZE}${level
+          ? `&reading-level=${level}`
+          : ''}`,
       ),
       fetch(
         `${env.bookApiUrl}/book-api/v1/books/${language ||
-          ''}?sort=-arrivaldate&page-size=${BOOKS_PAGE_SIZE}&reading-level=${level}`,
+          ''}?sort=-arrivaldate&page-size=${BOOKS_PAGE_SIZE}${level
+          ? `&reading-level=${level}`
+          : ''}`,
       ),
       fetch(`${env.bookApiUrl}/book-api/v1/levels`),
       fetch(`${env.bookApiUrl}/book-api/v1/languages`),
