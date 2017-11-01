fc7590ee5b2eaae4cebe0dd85e53a6a28aad77f5
Alexander Bjerkan <alb@knowit.no>
2017-11-01 08:58:13 +0100

    Add fetch specific methods
    Removes lots of string concatenations in pages, and rather use specific
    functions for fetching data. Looks way cleaner

--- env.js
+++ env.js
@@ -23,12 +23,12 @@ const gdlEnviroment = ENV.GDL_ENVIRONMENT || 'test';
 function getBookApiDomain(): string {
   switch (gdlEnviroment) {
     case 'prod':
-      return 'http://prod-proxy-658342484.eu-central-1.elb.amazonaws.com';
+      return 'http://prod-proxy-658342484.eu-central-1.elb.amazonaws.com/book-api/v1';
     case 'staging':
-      return 'http://staging-proxy-95967625.eu-central-1.elb.amazonaws.com';
+      return 'http://staging-proxy-95967625.eu-central-1.elb.amazonaws.com/book-api/v1';
     default:
       // test
-      return 'http://test-proxy-1865761686.eu-central-1.elb.amazonaws.com';
+      return 'http://test-proxy-1865761686.eu-central-1.elb.amazonaws.com/book-api/v1';
   }
 }
 
--- fetch.js
+++ fetch.js
@@ -7,12 +7,13 @@
  */
 
 import fetch from 'isomorphic-unfetch';
-import type { RemoteData } from './types';
+import { bookApiUrl } from './env';
+import type { RemoteData, Language, Book } from './types';
 
 /*
 * Wrap fetch with some error handling and automatic json parsing
 */
-export default async function(url: string): Promise<RemoteData<*>> {
+async function doFetch(url: string): Promise<RemoteData<any>> {
   try {
     const response = await fetch(url);
 
@@ -33,4 +34,60 @@ export default async function(url: string): Promise<RemoteData<*>> {
     }
     throw error;
   }
+}
+
+export default doFetch;
+
+// Default page size
+const PAGE_SIZE = 5;
+
+export function fetchLevels(
+  language: ?string,
+): Promise<RemoteData<Array<string>>> {
+  return doFetch(`${bookApiUrl}/levels/${language || ''}`);
+}
+
+export function fetchLanguages(): Promise<RemoteData<Array<Language>>> {
+  return doFetch(`${bookApiUrl}/languages`);
+}
+
+export function fetchEditorPicks(
+  language: ?string,
+): Promise<RemoteData<Array<Book>>> {
+  return doFetch(`${bookApiUrl}/editorpicks/${language || ''}`);
+}
+
+export function fetchBook(
+  id: string | number,
+  language: string,
+): Promise<RemoteData<Book>> {
+  return doFetch(`${bookApiUrl}/books/${language}/${id}`);
+}
+
+export function fetchSimilarBooks(
+  id: string | number,
+  language: string,
+): Promise<RemoteData<{ results: Array<Book> }>> {
+  return doFetch(
+    `${bookApiUrl}/books/${language}/similar/${id}?page-size=${PAGE_SIZE}`,
+  );
+}
+
+export function fetchJustArrivedBooks(
+  language: ?string,
+): Promise<RemoteData<{ results: Array<Book> }>> {
+  return doFetch(
+    `${bookApiUrl}/books/${language ||
+      ''}?sort=-arrivaldate&page-size=${PAGE_SIZE}`,
+  );
+}
+
+export function fetchBooksByLevel(
+  level: string,
+  language: ?string,
+): Promise<RemoteData<{ results: Array<Book> }>> {
+  return doFetch(
+    `${bookApiUrl}/books/${language ||
+      ''}?sort=-arrivaldate&page-size=${PAGE_SIZE}&reading-level=${level}`,
+  );
 }
