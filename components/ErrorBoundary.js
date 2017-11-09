f880a48a23905ebf546982c60e65314962dab28f
Alexander Bjerkan <alb@knowit.no>
2017-11-09 14:56:09 +0100

    Link on error page does full refresh
    So we don't try to gracefully error state in the ErrorBoundary component

--- components/ErrorBoundary.js
+++ components/ErrorBoundary.js
@@ -7,7 +7,6 @@
  */
 
 import * as React from 'react';
-import { Router } from '../routes';
 import ErrorComp from './Error';
 
 type Props = {
@@ -23,25 +22,9 @@ export default class ErrorBoundary extends React.Component<Props, State> {
     error: null,
   };
 
-  componentDidMount() {
-    Router.onRouteChangeStart = this.handleRouteChangeStart;
-  }
-
-  componentWillUnmount() {
-    Router.onChangeStart = null;
-  }
-
   componentDidCatch(error: Error) {
     this.setState({ error });
   }
-
-  // If we're showing the error page, and the route changes, reset the error state
-  // Allows us to navigate to landing page after the error
-  handleRouteChangeStart = () => {
-    if (this.state.error != null) {
-      this.setState({ error: null });
-    }
-  };
 
   render() {
     if (this.state.error) {
