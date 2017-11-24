d7c7e23a51d7df44ae95e1f581bb82c525d83062
Alexander Bjerkan <alb@knowit.no>
2017-11-24 09:37:45 +0100

    Chore: Upgrade dependencies

--- components/Error.js
+++ components/Error.js
@@ -2,7 +2,7 @@
 /**
  * Part of GDL gdl-frontend.
  * Copyright (C) 2017 GDL
- * 
+ *
  * See LICENSE
  */
 
@@ -19,57 +19,57 @@ type Props = {
   showNavbar: boolean,
 };
 
-export default class Error extends React.Component<Props> {
-  static defaultProps = {
-    showNavbar: true,
-  };
-
-  render() {
-    const { statusCode, showNavbar } = this.props;
-    // FIXME: lang prop in navbar here :/
-    return (
-      <div>
-        {showNavbar && <Navbar lang="eng" />}
-        <Container mt={[15, 40]}>
-          <Card px={17} py={24} textAlign="center">
-            <H1>
-              {statusCode === 404 ? (
-                <Trans>This page could not be found</Trans>
-              ) : (
-                <Trans>An unexpected error has occurred</Trans>
-              )}
-            </H1>
-            {statusCode && (
-              <Card
-                fontSize={[40, 60]}
-                h={[188, 247]}
-                w={[152, 200]}
-                mx="auto"
-                mb={20}
-                style={{
-                  background: '#DA3D34',
-                  color: '#fff',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}
-              >
-                {statusCode}
-              </Card>
-            )}
-            {typeof window !== 'undefined' && window.history.length > 1 ? (
-              // eslint-disable-next-line jsx-a11y/anchor-is-valid
-              <A href="" onClick={() => window.history.back()}>
-                <Trans>Go back</Trans>
-              </A>
+const Error = (props: Props) => {
+  const { statusCode, showNavbar } = props;
+  // FIXME: lang prop in navbar here :/
+  return (
+    <div>
+      {showNavbar && <Navbar lang="eng" />}
+      <Container mt={[15, 40]}>
+        <Card px={17} py={24} textAlign="center">
+          <H1>
+            {statusCode === 404 ? (
+              <Trans>This page could not be found</Trans>
             ) : (
-              <A href="/">
-                <Trans>Go to start page</Trans>
-              </A>
+              <Trans>An unexpected error has occurred</Trans>
             )}
-          </Card>
-        </Container>
-      </div>
-    );
-  }
-}
+          </H1>
+          {statusCode && (
+            <Card
+              fontSize={[40, 60]}
+              h={[188, 247]}
+              w={[152, 200]}
+              mx="auto"
+              mb={20}
+              style={{
+                background: '#DA3D34',
+                color: '#fff',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              {statusCode}
+            </Card>
+          )}
+          {typeof window !== 'undefined' && window.history.length > 1 ? (
+            // eslint-disable-next-line jsx-a11y/anchor-is-valid
+            <A href="" onClick={() => window.history.back()}>
+              <Trans>Go back</Trans>
+            </A>
+          ) : (
+            <A href="/">
+              <Trans>Go to start page</Trans>
+            </A>
+          )}
+        </Card>
+      </Container>
+    </div>
+  );
+};
+
+Error.defaultProps = {
+  showNavbar: true,
+};
+
+export default Error;
