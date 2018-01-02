a043dbf30ad3f6d3273d88fa3bebd06426656c05
Runar Furenes <ruf@knowit.no>
2018-01-02 12:24:25 +0100

    #146: async loading of google analytics GlobalDigitalLibraryio/issues#146

--- __tests__/pages/_document.test.js
+++ __tests__/pages/_document.test.js
@@ -20,7 +20,7 @@ test('Sets global GDL environment variable, defaults to \'test\'', () => {
 test('Adds Google Analytics, defaults to \'N/A\' because we don\'t have an account at Google for env=local' , () => {
   const tree = shallow(<Document />);
   expect(tree.find('script').at(1).html()).toEqual(
-    '<script>\n' +
+    '<script async="">\n' +
     '                (function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){\n' +
     '                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n' +
     '                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n' +
