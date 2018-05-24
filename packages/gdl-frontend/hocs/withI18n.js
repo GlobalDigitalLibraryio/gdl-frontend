98fa757bb5594112079cf91be642567d9d7b185d
Alexander Bjerkan <alb@knowit.no>
2018-05-24 07:13:46 +0200

    Simplify i18n as we currently only support one language

--- packages/gdl-frontend/hocs/withI18n.js
+++ packages/gdl-frontend/hocs/withI18n.js
@@ -8,19 +8,12 @@
 
 import * as React from 'react';
 import { I18nProvider, withI18n } from '@lingui/react';
-import Head from 'next/head';
-import Url from 'domurl';
-import type { Context } from '../types';
-import { canonical } from '../config';
+
 // Currently we only support English
 import catalog from '../locale/en/messages';
 
 type Props = {
-  language: string,
-  catalog: string,
-  url?: {
-    asPath: string
-  }
+  language: string
 };
 
 // For now we have to add each translation here
@@ -31,75 +24,28 @@ const translations = {
 /**
  * A HoC that that faciliates our i18n layer
  */
-export default (Page: React.ComponentType<any>) => {
-  const I18nPage = withI18n()(Page);
+export default (App: React.ComponentType<any>) => {
+  const AppWithI18n = withI18n()(App);
 
-  return class PageWithI18n extends React.Component<Props> {
-    static async getInitialProps(context: Context) {
-      // Evaluate the composed component's getInitialProps()
-      let composedInitialProps;
-      // Check if it actually is a next page
-      if (typeof Page.getInitialProps === 'function') {
-        composedInitialProps = await Page.getInitialProps(context);
-      }
+  return class I18n extends React.Component<Props> {
+    static displayName = 'withI18n(App)';
 
-      // et the 'language' from the request object on the server.
-      // In the browser, use the same values that the server serialized.
-      const { req } = context;
-      // $FlowFixMe: How to handle that we get 'language' on the request object?
-      let { language } = req || window.__NEXT_DATA__.props;
-
-      // Fallback to english if the language isn't found in the translations object
-      if (!(language in translations)) {
-        language = 'en';
-      }
-
-      return {
-        ...composedInitialProps,
-        language
-      };
-    }
+    static getInitialProps = App.getInitialProps;
 
     render() {
-      const languages = Object.keys(translations);
-      const { language, ...props } = this.props;
-      // Make sure URL exists here. If there is a 404 it doesn't
-      const url = props.url && new Url(`${canonical}/${props.url.asPath}`);
-      if (url) {
-        delete url.query.hl;
-      }
       // Wrap our page with the i18n provider and add alternate links to the other supported languages in the head
+
+      // Currently we only support English
+      const language = 'en';
 
       return (
         <I18nProvider
-          language={language}
+          language="en"
           catalogs={{
             [language]: translations[language]
           }}
         >
-          <Head>
-            {url && (
-              <React.Fragment>
-                <link
-                  href={url.toString()}
-                  hrefLang="x-default"
-                  rel="alternate"
-                />
-                {languages.map(lang => {
-                  url.query.hl = lang;
-                  return (
-                    <link
-                      key={lang}
-                      href={url.toString()}
-                      hrefLang={lang}
-                      rel="alternate"
-                    />
-                  );
-                })}
-              </React.Fragment>
-            )}
-          </Head>
-          <I18nPage {...props} />
+          <AppWithI18n {...this.props} />
         </I18nProvider>
       );
     }
