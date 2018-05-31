e9c7006236a863866a2d14dadd43b11b5d326fb7
Alexander Bjerkan <alb@knowit.no>
2018-05-31 11:30:33 +0200

    Attempt

--- packages/gdl-frontend/components/GdlThemeProvider.js
+++ packages/gdl-frontend/components/GdlThemeProvider.js
@@ -7,9 +7,13 @@
  */
 
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import { ThemeProvider } from 'emotion-theming';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import JssProvider from 'react-jss/lib/JssProvider';
 
+import getPageContext from '../getPageContext';
+import type { Context } from '../types';
 import { misc } from '../style/theme';
 
 const theme = { space: [], breakpoints: misc.breakpoints };
@@ -18,20 +22,37 @@ const theme = { space: [], breakpoints: misc.breakpoints };
  *  Setup the theme of our application
  */
 export default class GdlThemeProvider extends React.Component<*> {
-  static childContextTypes = {
-    reactIconBase: PropTypes.object
-  };
+  constructor(props, context: Context) {
+    super(props, context);
+    this.pageContext = this.props.pageContext || getPageContext();
+  }
 
-  // Define icon size
-  getChildContext() {
-    return {
-      reactIconBase: {
-        size: misc.iconSize
-      }
-    };
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
   }
 
   render() {
-    return <ThemeProvider theme={theme} {...this.props} />;
+    return (
+      <ThemeProvider theme={theme}>
+        <JssProvider
+          jss={this.pageContext.jss}
+          registry={this.pageContext.sheetsRegistry}
+          generateClassName={this.pageContext.generateClassName}
+        >
+          <>
+            <CssBaseline />
+            <MuiThemeProvider
+              theme={this.pageContext.theme}
+              sheetsManager={this.pageContext.sheetsManager}
+              {...this.props}
+            />
+          </>
+        </JssProvider>
+      </ThemeProvider>
+    );
   }
 }
