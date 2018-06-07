281915e8b938b68bc4546ebff9cc97461024e7f7
Runar Furenes <ruf@knowit.no>
2018-06-07 08:29:34 +0200

    #358: Remove references to config.js GlobalDigitalLibraryio/issues#358

--- packages/admin-frontend/server/server.js
+++ packages/admin-frontend/server/server.js
@@ -9,13 +9,9 @@
 const express = require('express');
 const next = require('next');
 const cookieParser = require('cookie-parser');
-const config = require('../config');
 
 const isDev = process.env.NODE_ENV !== 'production';
 const app = next({ dev: isDev });
-
-console.log('> GDL environment: ', config.GDL_ENVIRONMENT);
-console.log('> Will report errors: ', config.REPORT_ERRORS);
 
 async function setup() {
   await app.prepare();
