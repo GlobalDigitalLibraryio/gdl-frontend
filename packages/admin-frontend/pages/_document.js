219dddbb90130258186b1a60b961f8d4486f2b12
Runar Furenes <ruf@knowit.no>
2018-06-11 09:47:33 +0200

    #348: Don't use /admin prefix for css in dev-mode GlobalDigitalLibraryio/issues#348

--- packages/admin-frontend/pages/_document.js
+++ packages/admin-frontend/pages/_document.js
@@ -1,11 +1,14 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 
+const isDev = process.env.NODE_ENV !== 'production';
+const cssPath = (isDev ? "/" : "/admin/") + "_next/static/style.css";
+
 export default class MyDocument extends Document {
   render() {
     return (
       <html>
         <Head>
-          <link rel="stylesheet" href="/admin/_next/static/style.css" />
+          <link rel="stylesheet" href={cssPath} />
         </Head>
         <body>
           <Main />
