f8dac3b889d8875f50b1cfc79b50f5fadd1d2f40
Marius Kohmann <marius.kohmann@gmail.com>
2018-06-13 12:27:24 +0200

    add fetch chapter functions

--- packages/admin-frontend/lib/fetch.js
+++ packages/admin-frontend/lib/fetch.js
@@ -92,6 +92,19 @@ export async function saveBook(
   return result;
 }
 
+export async function fetchChapter(
+    bookId: string | number,
+    chapterId: string | number,
+    language: string
+): Promise<RemoteData<Chapter>> {
+    const result = await doFetch(
+        `${bookApiUrl}/books/${language}/${bookId}/chapters/${chapterId}`
+    );
+
+    return result;
+}
+
+
 export async function saveChapter(
   book: BookDetails,
   chapter: Chapter
