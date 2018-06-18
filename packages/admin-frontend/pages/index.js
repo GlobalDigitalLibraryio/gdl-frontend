e7df486c6fa9a850ac3ec199f1f82efe47a20611
Marius Kohmann <marius.kohmann@gmail.com>
2018-06-18 09:27:27 +0200

    wrapping links in div

--- packages/admin-frontend/pages/index.js
+++ packages/admin-frontend/pages/index.js
@@ -14,13 +14,17 @@ class IndexPage extends React.Component<{}> {
     return (
       <div>
         <h1>GDL Admin</h1>
-        <Link prefetch href="/admin/crop">
-          <a>Crop images</a>
-        </Link>
-        <br />
-        <Link prefetch href="/admin/edit">
-          <a>Edit book</a>
-        </Link>
+
+        <div>
+          <Link prefetch href="/admin/crop">
+            <a>Crop images</a>
+          </Link>
+        </div>
+        <div>
+          <Link prefetch href="/admin/edit">
+            <a>Edit book</a>
+          </Link>
+        </div>
       </div>
     );
   }
