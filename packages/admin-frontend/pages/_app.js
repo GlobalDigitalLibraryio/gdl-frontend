e1e12e862500225dd97dacf070930b64b375ba5f
Alexander Bjerkan <alb@knowit.no>
2018-06-18 13:28:54 +0200

    Add claim check to admin

---
+++ packages/admin-frontend/pages/_app.js
@@ -0,0 +1,32 @@
+// @flow
+/**
+ * Part of GDL gdl-frontend.
+ * Copyright (C) 2018 GDL
+ *
+ * See LICENSE
+ */
+
+import React from 'react';
+import NextApp, { Container as NextContainer } from 'next/app';
+
+class App extends NextApp {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <NextContainer>
+        <Component {...pageProps} />
+      </NextContainer>
+    );
+  }
+}
+
+export default App;
