dadd5a385d8a37d2e4d9bcb61affa800981450a4
Alexander Bjerkan <alb@knowit.no>
2018-06-20 14:25:31 +0200

    spell fix

--- packages/gdl-auth/claims.js
+++ packages/gdl-auth/claims.js
@@ -1,7 +1,7 @@
 // @flow
 import { GDL_ENVIRONMENT } from 'gdl-config';
 
-// If we'e running locally, we're probably developing and using the test environment APIs
+// If we're running locally, we're probably developing and using the test environment APIs
 // So for the purpose of checking user access, we use 'test', otherwise we can't access anything...
 const ENV = GDL_ENVIRONMENT === 'dev' ? 'test' : GDL_ENVIRONMENT;
 
