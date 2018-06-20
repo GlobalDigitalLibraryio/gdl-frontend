153b23168e4d1aa47ad5bb84f0b37ce0f98262d7
Alexander Bjerkan <alb@knowit.no>
2018-06-20 14:09:34 +0200

    Use TEST for access check while developing
    Simplifies local development considerably

--- packages/gdl-auth/claims.js
+++ packages/gdl-auth/claims.js
@@ -1,8 +1,12 @@
 // @flow
 import { GDL_ENVIRONMENT } from 'gdl-config';
 
+// If we'e running locally, we're probably developing and using the test environment APIs
+// So for the purpose of checking user access, we use 'test', otherwise we can't access anything...
+const ENV = GDL_ENVIRONMENT === 'dev' ? 'test' : GDL_ENVIRONMENT;
+
 export default {
-  readAdmin: `admin-${GDL_ENVIRONMENT}:read`,
-  writeBook: `books-${GDL_ENVIRONMENT}:write`,
-  writeImage: `images-${GDL_ENVIRONMENT}:write`
+  readAdmin: `admin-${ENV}:read`,
+  writeBook: `books-${ENV}:write`,
+  writeImage: `images-${ENV}:write`
 };
