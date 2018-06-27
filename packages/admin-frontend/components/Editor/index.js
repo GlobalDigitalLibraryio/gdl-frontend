e27be05ba0e9ee32aad12d88c0407c1fea8bde50
Elena Andrés <elenaandres08@gmail.com>
2018-06-27 09:18:58 +0200

    Issue#351 self service for flagging books (#182)
    * #351: drop down menu to change publishing status
    
    * #351: removed button from form for changing publishing status
    
    * #351: removed unused imports
    
    * #351: ignored flow and added some styling to edit page
    
    * #351: Removed publishingStatus from state
    
    * #351: Added some styling to edit page
    
    * #351: prettier

--- packages/admin-frontend/components/Editor/index.js
+++ packages/admin-frontend/components/Editor/index.js
@@ -6,12 +6,14 @@
  * See LICENSE
  */
 
-import React, { Fragment } from 'react';
+import React from 'react';
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
 
-import type { BookDetails, Chapter } from '../../types';
+import type { BookDetails, Chapter, PublishingStatus } from '../../types';
 import { saveBook, saveChapter } from '../../lib/fetch';
+import Container from '../Container';
+import { FormControl, InputLabel, Select, Typography } from '@material-ui/core';
 
 // ReactJson doesn't work on the server, so make sure we lazy load it
 const ReactJson = dynamic(import('react-json-view'));
@@ -19,19 +21,29 @@ const ReactJson = dynamic(import('react-json-view'));
 // Only allow these fields to be edited
 const EDITABLE_CHAPTER_FIELDS = ['content', 'chapterType'];
 const EDITABLE_BOOK_FIELDS = ['title', 'description', 'pageOrientation'];
+const publishingStatus = ['PUBLISHED', 'FLAGGED', 'UNLISTED'];
+
+type State = {
+  isSaving: boolean,
+  book: BookDetails,
+  didSave: boolean,
+  chapter: ?Chapter,
+  selectedPublishingStatus: PublishingStatus
+};
 
 export default class Editor extends React.Component<
   {
     book: BookDetails,
     chapter?: Chapter
   },
-  { isSaving: boolean, book: BookDetails, didSave: boolean, chapter: ?Chapter }
+  State
 > {
   state = {
     isSaving: false,
     didSave: false,
     book: this.props.book,
-    chapter: this.props.chapter
+    chapter: this.props.chapter,
+    selectedPublishingStatus: this.props.book.publishingStatus
   };
 
   handleChapterEdit = (edit: {
@@ -66,19 +78,32 @@ export default class Editor extends React.Component<
     this.setState({ isSaving: false, didSave: true });
   };
 
+  handlePublishingStatusChange = (event: SyntheticInputEvent<EventTarget>) => {
+    // $FlowFixMe: ignore flow
+    const selectedPublishingStatus: PublishingStatus = event.target.value;
+
+    this.setState(state => ({
+      selectedPublishingStatus: selectedPublishingStatus,
+      // $FlowFixMe: ignore flow
+      book: {
+        ...state.book,
+        publishingStatus: selectedPublishingStatus
+      }
+    }));
+  };
+
   render() {
     const { book, chapter } = this.props;
-
     return (
-      <Fragment>
-        <h1>
+      <Container>
+        <Typography variant="headline" component="h1" gutterBottom>
           Editing book:{' '}
           <Link href={`/${book.language.code}/books/details/${book.id}`}>
             <a>{book.title}</a>
           </Link>
-        </h1>
+        </Typography>
         {this.props.chapter && (
-          <h2>
+          <Typography variant="headline" component="h2" gutterBottom>
             Editing chapter:{' '}
             <Link
               href={`/${book.language.code}/books/read/${book.id}/${
@@ -87,7 +112,7 @@ export default class Editor extends React.Component<
             >
               <a>{this.props.chapter.seqNo}</a>
             </Link>
-          </h2>
+          </Typography>
         )}
 
         <div style={{ textAlign: 'right' }}>
@@ -113,7 +138,32 @@ export default class Editor extends React.Component<
           displayObjectSize={false}
           displayDataTypes={false}
         />
-      </Fragment>
+
+        <Typography
+          variant="headline"
+          component="h2"
+          gutterBottom
+          css={{ marginTop: '30px' }}
+        >
+          Edit Publishing Status
+        </Typography>
+        <form>
+          <FormControl>
+            <InputLabel>Publishing status</InputLabel>
+            <Select
+              native
+              value={this.state.selectedPublishingStatus}
+              onChange={this.handlePublishingStatusChange}
+            >
+              {publishingStatus.map(status => (
+                <option key={status} value={status}>
+                  {status}
+                </option>
+              ))}
+            </Select>
+          </FormControl>
+        </form>
+      </Container>
     );
   }
 }
