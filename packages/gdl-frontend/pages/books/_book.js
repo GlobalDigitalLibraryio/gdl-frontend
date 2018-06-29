f927de3315dc5ee0b6fbec435093025fbfd095f9
Elena Andres <elenaandres08@gmail.com>
2018-06-29 09:51:56 +0200

    #404 Added link to read book when clicking on book cover

--- packages/gdl-frontend/pages/books/_book.js
+++ packages/gdl-frontend/pages/books/_book.js
@@ -145,11 +145,20 @@ class BookPage extends React.Component<Props, { anchorEl: ?HTMLElement }> {
             <View flexDirection="row" mt={['135px', spacing.medium]}>
               <CoverWrap>
                 <View>
-                  <BookCover
-                    coverImage={book.coverImage}
-                    w={[130, 260]}
-                    h={[175, 365]}
-                  />
+                  <Link
+                    route="read"
+                    passHref
+                    params={{ id: book.id, lang: book.language.code }}
+                    prefetch
+                  >
+                    <a title={book.title} aria-hidden>
+                      <BookCover
+                        coverImage={book.coverImage}
+                        w={[130, 260]}
+                        h={[175, 365]}
+                      />
+                    </a>
+                  </Link>
                   {this.props.userHasEditImageAccess && (
                     <NextLink
                       href={{
