b6563608412895586407bb8136663c6c46e2f892
Marius Kohmann <marius.kohmann@gmail.com>
2018-07-03 09:08:01 +0200

    configure webpack-bundle-analyzer

--- packages/gdl-frontend/next.config.js
+++ packages/gdl-frontend/next.config.js
@@ -1,9 +1,19 @@
-// @flow
-
-// Add source maps in produciton for Sentry
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { ANALYZE } = process.env;
 const withSourceMaps = require('@zeit/next-source-maps');
+
 module.exports = withSourceMaps({
   webpack(config, options) {
+    if (ANALYZE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'server',
+          analyzerPort: options.isServer ? 8888 : 8889,
+          openAnalyzer: true
+        })
+      );
+    }
+
     return config;
   }
 });
