525775b5e519211384009b6b10024b187f2fbad7
Marius Kohmann <marius.kohmann@gmail.com>
2018-07-05 12:25:57 +0200

    remove json-editor dependency

---
+++ packages/admin-frontend/media.js
@@ -0,0 +1,34 @@
+// @flow
+/**
+ * Part of GDL gdl-frontend.
+ * Copyright (C) 2017 GDL
+ *
+ * See LICENSE
+ */
+
+import { css } from 'react-emotion';
+import { TABLET_BREAKPOINT } from './theme/misc';
+
+/**
+ * Mobile first media template
+ *
+ * (Also has a mobile only query)
+ *
+ */
+
+// A function returning a function :)
+const query = (condition: 'min' | 'max', width: number) => (
+  // $FlowFixMe Flow doesn't play nice with template literals https://github.com/facebook/flow/issues/2616
+  ...args
+) => css`
+  @media (${condition}-width: ${width}px) {
+    ${css(...args)};
+  }
+`;
+
+const media = {
+  mobile: query('max', TABLET_BREAKPOINT - 1),
+  tablet: query('min', TABLET_BREAKPOINT)
+};
+
+export default media;
