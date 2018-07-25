cccbd6c47d5f520f2cdd8549d54dac9edb7b779c
Elena <elenaandres08@gmail.com>
2018-07-25 15:18:49 +0200

    #422: Footer with links and social media icons

--- packages/gdl-frontend/components/Layout/index.js
+++ packages/gdl-frontend/components/Layout/index.js
@@ -9,7 +9,7 @@
 import React, { type Node } from 'react';
 import styled from 'react-emotion';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import { Paper, CardContent } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
 import { Trans } from '@lingui/react';
 
 import CCLogo from '../GlobalMenu/cc-logo.svg';
@@ -18,8 +18,14 @@ import type { Category } from '../../types';
 import Navbar from '../Navbar';
 import GlobalMenu from '../GlobalMenu';
 import { misc, colors } from '../../style/theme';
+import media from '../../style/media';
+
 import { classRoomTheme } from '../../getPageContext';
 import Container from '../../elements/Container';
+
+import FaFacebookSquare from 'react-icons/lib/fa/facebook-square';
+import FaTwitterSquare from 'react-icons/lib/fa/twitter-square';
+import FaGithubSquare from 'react-icons/lib/fa/github-square';
 
 const Main = styled(Paper)`
   background: ${colors.container.background};
@@ -37,8 +43,11 @@ const PageWrapper = styled('div')`
 `;
 
 const Footer = styled('footer')`
-  background: #eff0f2;
-  background: #e3e3e3;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 8px;
   a {
     text-decoration: none;
     color: #444;
@@ -51,11 +60,28 @@ const Footer = styled('footer')`
 export const LinkGrid = styled('div')`
   font-size: 0.8rem;
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   grid-row-gap: 13px;
   grid-column-gap: 26px;
   margin-top: 20px;
   margin-bottom: 20px;
+  ${media.tablet`
+    width: 100%;
+    grid-template-columns: repeat(6, 1fr);
+  `};
+`;
+
+export const SocialMediaIcons = styled('div')`
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+  a {
+    font-size: 2.25rem;
+    color: #555;
+    &:hover {
+      color: ${colors.default};
+    }
+  }
 `;
 
 type Props = {|
@@ -93,39 +119,66 @@ class Layout extends React.Component<Props, State> {
           onClose={() => this.setState({ drawerIsOpen: false })}
           isOpen={this.state.drawerIsOpen}
         />
-        {wrapWithMain ? <Main component="main">{children}</Main> : children}
-        <Footer>
-          <Container size="large">
-            <CardContent css={{ textAlign: 'center' }}>
-              <a
-                href="https://creativecommons.org/"
-                component="a"
-                aria-label="Creative Commons"
-              >
-                <CCLogo css={{ width: '100px' }} />
+        {wrapWithMain ? (
+          <Main square component="main">
+            {children}
+          </Main>
+        ) : (
+          children
+        )}
+        <Container size="large" stickToEdgeOnLargeScreens width="100%">
+          <Footer>
+            <LinkGrid>
+              <a href="https://home.digitallibrary.io/the-global-digital-library-uses-cookies/">
+                <Trans>Cookie policy</Trans>
               </a>
-            </CardContent>
-            <CardContent>
+              <a href="https://home.digitallibrary.io/privacy/">
+                <Trans>Privacy policy</Trans>
+              </a>
+              <a href="https://blog.digitallibrary.io/cc/">
+                <Trans>Licensing and reuse</Trans>
+              </a>
               <a href={config.zendeskUrl}>
-                <Trans>Found something? Report issues</Trans>
+                <Trans>Report issues</Trans>
               </a>
-              <LinkGrid>
-                <a href="https://home.digitallibrary.io/about/">
-                  <Trans>About the GDL</Trans>
-                </a>
-                <a href="https://home.digitallibrary.io/the-global-digital-library-uses-cookies/">
-                  <Trans>Cookie policy</Trans>
-                </a>
-                <a href="https://home.digitallibrary.io/privacy/">
-                  <Trans>Privacy policy</Trans>
-                </a>
-                <a href="https://blog.digitallibrary.io/cc/">
-                  <Trans>Licensing and reuse</Trans>
-                </a>
-              </LinkGrid>
-            </CardContent>
-          </Container>
-        </Footer>
+
+              <a href="https://home.digitallibrary.io/about/">
+                <Trans>About</Trans>
+              </a>
+              <a href="https://blog.digitallibrary.io/">
+                <Trans>Blog</Trans>
+              </a>
+            </LinkGrid>
+
+            <div
+              css={{
+                border: '1px solid grey',
+                width: '100%',
+                margin: '10px'
+              }}
+            />
+
+            <a
+              href="https://creativecommons.org/"
+              component="a"
+              aria-label="Creative Commons"
+            >
+              <CCLogo css={{ width: '100px' }} />
+            </a>
+
+            <SocialMediaIcons>
+              <a href="https://nb-no.facebook.com/globaldigitallibrary/">
+                <FaFacebookSquare />
+              </a>
+              <a href="">
+                <FaTwitterSquare />
+              </a>
+              <a href="">
+                <FaGithubSquare />
+              </a>
+            </SocialMediaIcons>
+          </Footer>
+        </Container>
       </PageWrapper>
     );
   }
