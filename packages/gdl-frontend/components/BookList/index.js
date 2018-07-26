1ba9733974a27e3ce97912ff4a6ba87ba2d8ce9e
Marius Kohmann <marius.kohmann@gmail.com>
2018-07-26 15:53:04 +0200

    prettier

--- packages/gdl-frontend/components/BookList/index.js
+++ packages/gdl-frontend/components/BookList/index.js
@@ -37,7 +37,11 @@ const BookList = ({ books, heading, browseLinkProps }: Props) => (
       </Typography>
       {browseLinkProps && (
         <BrowseLink {...browseLinkProps}>
-          <Button variant="outlined" color="primary" css={{backgroundColor: "#fff", color: "#0277bd"}}>
+          <Button
+            variant="outlined"
+            color="primary"
+            css={{ backgroundColor: '#fff', color: '#0277bd' }}
+          >
             <Trans>More</Trans>
           </Button>
         </BrowseLink>
