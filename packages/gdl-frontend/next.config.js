45e4318b4ffe7e9d441edd113bf61d870fa1e8d7
Elena <elenaandres08@gmail.com>
2018-08-03 13:33:14 +0200

    Only one module.export in next config

--- packages/gdl-frontend/next.config.js
+++ packages/gdl-frontend/next.config.js
@@ -1,25 +1,26 @@
 // @flow
-const withTM = require('@weco/next-plugin-transpile-modules');
 
 // Add source maps in production for Sentry
 const withSourceMaps = require('@zeit/next-source-maps');
+const withTM = require('@weco/next-plugin-transpile-modules');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { ANALYZE } = process.env;
 
-module.exports = withSourceMaps({
-  webpack(config, options) {
-    if (ANALYZE) {
-      config.plugins.push(
-        new BundleAnalyzerPlugin({
-          analyzerMode: 'server',
-          analyzerPort: options.isServer ? 8888 : 8889,
-          openAnalyzer: true
-        })
-      );
+module.exports = withTM(
+  withSourceMaps({
+    transpileModules: ['gdl-auth', 'gdl-config'],
+    webpack(config, options) {
+      if (ANALYZE) {
+        config.plugins.push(
+          new BundleAnalyzerPlugin({
+            analyzerMode: 'server',
+            analyzerPort: options.isServer ? 8888 : 8889,
+            openAnalyzer: true
+          })
+        );
+      }
+
+      return config;
     }
-
-    return config;
-  }
-});
-
-module.exports = withTM({ transpileModules: ['gdl-auth', 'gdl-config'] });
+  })
+);
