98afcb540be1238a4271084fb8ec8bda22c68f96
Alexander Bjerkan <alb@knowit.no>
2018-08-23 09:28:18 +0200

    Display the same book cover in book edit form as when cropping it

--- packages/admin-frontend/components/EditBook/EditBookImage.js
+++ packages/admin-frontend/components/EditBook/EditBookImage.js
@@ -35,7 +35,9 @@ export default class EditBookImage extends React.Component<Props, State> {
             src={
               // Adds the storedRatio parameter to get the latest crop-parameters of the cover image.
               // The timestamp parameter needs to be added to cache bust the CDN.
-              book.coverImage.url + '?storedRatio=0.81&timestamp=' + Date.now()
+              book.coverImage.url +
+              '?storedRatio=0.81&focalX=50&focalY=50&ratio=0.81&timestamp=' +
+              Date.now()
             }
             alt="Cover"
             width={260}
