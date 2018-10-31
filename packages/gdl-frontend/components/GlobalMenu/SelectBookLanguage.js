98d38d6ff34f65928e8ac9f52f664e54eaa0404c
Alexander Bjerkan <alb@knowit.no>
2018-10-31 15:10:56 +0100

    Improve error handling and spinners for menus

---
+++ packages/gdl-frontend/components/GlobalMenu/SelectBookLanguage.js
@@ -0,0 +1,121 @@
+// @flow
+/**
+ * Part of GDL gdl-frontend.
+ * Copyright (C) 2018 GDL
+ *
+ * See LICENSE
+ */
+
+import React, { type Node } from 'react';
+import { Trans } from '@lingui/react';
+import { Drawer, Typography } from '@material-ui/core';
+
+import type { Language } from '../../types';
+import { fetchLanguages } from '../../fetch';
+import LanguageList from '../LanguageList';
+import { getBookLanguageCode } from '../../lib/storage';
+
+type Props = {
+  anchor?: 'left' | 'right',
+  children: (data: { onClick: () => void, loading: boolean }) => Node,
+  onSelectLanguage?: Language => void
+};
+
+type State = {
+  showMenu: boolean,
+  languages: ?'LOADING' | 'ERROR' | Array<Language>,
+  selectedLanguage: string
+};
+
+function linkProps(language) {
+  return {
+    route: 'books',
+    params: { lang: language.code }
+  };
+}
+
+/**
+ * We cache the results of the successful API call, since this doesn't really change
+ */
+let cache = undefined;
+
+export default class SelectBookLanguage extends React.Component<Props, State> {
+  state = {
+    showMenu: false,
+    languages: cache,
+    selectedLanguage: getBookLanguageCode()
+  };
+
+  /**
+   * We only load the book languages if the menu is "open" and we're in an uninitialized state
+   */
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    if (this.state.showMenu && this.state.languages == null) {
+      this.loadLanguages();
+    }
+
+    // Makes sure we always show the correct language as selected when the menu is opened
+    // When React Hooks arrive this will be so much nicer
+    const selectedLanguage = getBookLanguageCode();
+    if (this.state.selectedLanguage !== selectedLanguage) {
+      this.setState({
+        selectedLanguage
+      });
+    }
+  }
+
+  async loadLanguages() {
+    this.setState({ languages: 'LOADING' });
+    const result = await fetchLanguages();
+
+    if (result.isOk) {
+      this.setState({
+        languages: result.data
+      });
+      cache = result.data;
+    } else {
+      this.setState({ languages: 'ERROR' });
+    }
+  }
+
+  handleSelectLanguage = (language: Language) => {
+    this.handleCloseMenu();
+    this.props.onSelectLanguage && this.props.onSelectLanguage(language);
+  };
+
+  handleShowMenu = () => this.setState({ showMenu: true });
+
+  handleCloseMenu = () => this.setState({ showMenu: false });
+
+  render() {
+    const { children, anchor } = this.props;
+    const { showMenu, languages, selectedLanguage } = this.state;
+    return (
+      <>
+        {children({
+          onClick: this.handleShowMenu,
+          loading: languages === 'LOADING'
+        })}
+        <Drawer
+          onClose={this.handleCloseMenu}
+          open={showMenu && languages !== 'LOADING'}
+          anchor={anchor}
+        >
+          {languages === 'ERROR' && (
+            <Typography component="span" color="error" css={{ margin: '1rem' }}>
+              <Trans>Error loading data.</Trans>
+            </Typography>
+          )}
+          {Array.isArray(languages) && (
+            <LanguageList
+              onSelectLanguage={this.handleSelectLanguage}
+              selectedLanguageCode={selectedLanguage}
+              linkProps={linkProps}
+              languages={languages}
+            />
+          )}
+        </Drawer>
+      </>
+    );
+  }
+}
---
+++ packages/gdl-frontend/components/LanguageList.js
@@ -0,0 +1,109 @@
+// @flow
+/**
+ * Part of GDL gdl-frontend.
+ * Copyright (C) 2018 GDL
+ *
+ * See LICENSE
+ */
+
+import React from 'react';
+import { Trans } from '@lingui/react';
+import {
+  Divider,
+  List,
+  ListSubheader,
+  ListItem,
+  ListItemText,
+  ListItemIcon
+} from '@material-ui/core';
+import { Check as CheckIcon } from '@material-ui/icons';
+
+import { Link } from '../routes';
+import SrOnly from './SrOnly';
+import type { Language } from '../types';
+import { colors } from '../style/theme';
+
+type Props = {
+  selectedLanguageCode: ?string,
+  onSelectLanguage: Language => void,
+  languages: Array<Language>,
+  linkProps?: (language: Language) => {}
+};
+
+const LanguageList = ({
+  languages,
+  onSelectLanguage,
+  selectedLanguageCode,
+  linkProps
+}: Props) => {
+  const selectedLanguage = languages.find(l => l.code === selectedLanguageCode);
+
+  // We don't want to display the seelected language in the list
+  const filteredLanguages = selectedLanguage
+    ? languages.filter(l => l !== selectedLanguage)
+    : languages;
+
+  return (
+    <List
+      component="div"
+      subheader={
+        <ListSubheader component="div">
+          <Trans>Choose book language</Trans>
+        </ListSubheader>
+      }
+    >
+      {selectedLanguage && (
+        <>
+          <ListItem>
+            <ListItemIcon>
+              <CheckIcon css={{ color: colors.base.green }} />
+            </ListItemIcon>
+            <ListItemText inset>
+              <Trans>
+                <SrOnly>Selected: </SrOnly>
+                {selectedLanguage.name}
+              </Trans>
+            </ListItemText>
+          </ListItem>
+          <Divider />
+        </>
+      )}
+      {filteredLanguages.map(l => (
+        <LanguageItem
+          key={l.code}
+          language={l}
+          linkProps={linkProps}
+          onSelectLanguage={onSelectLanguage}
+        />
+      ))}
+    </List>
+  );
+};
+
+const LanguageItem = ({ language, linkProps, onSelectLanguage }) => {
+  if (linkProps) {
+    return (
+      <Link key={language.code} passHref {...linkProps(language)}>
+        <ListItem
+          button
+          component="a"
+          onClick={() => onSelectLanguage(language)}
+        >
+          <ListItemText inset>{language.name}</ListItemText>
+        </ListItem>
+      </Link>
+    );
+  }
+
+  return (
+    <ListItem
+      key={language.code}
+      onClick={() => onSelectLanguage(language)}
+      button
+    >
+      <ListItemText inset>{language.name}</ListItemText>
+    </ListItem>
+  );
+};
+
+export default LanguageList;
