76d1e0582436f75b2958024725ef342c121eee1e
Kien Vu <kienv91@gmail.com>
2018-11-23 09:30:30 +0100

    moved crowdin script to load under Head

--- packages/gdl-frontend/components/InContextTranslation.js
+++ packages/gdl-frontend/components/InContextTranslation.js
@@ -10,6 +10,7 @@ import React, { type Node } from 'react';
 import { CircularProgress } from '@material-ui/core';
 import styled from 'react-emotion';
 import { fetchMyTranslations } from '../fetch';
+import Head from 'next/head';
 
 type Props = {
   bookId: string,
@@ -20,6 +21,11 @@ type Props = {
 type State = {
   loading: boolean
 };
+
+// In-context requires that jipt is defined before the crowdin script is initialized
+if (typeof window !== 'undefined') {
+  window._jipt = [];
+}
 
 class InContextTranslation extends React.Component<Props, State> {
   state = {
@@ -33,11 +39,6 @@ class InContextTranslation extends React.Component<Props, State> {
     const toLanguage = await this.loadTranslation();
     await initInContext(project.en, toLanguage);
 
-    const script = document.createElement('script');
-    script.src = 'https://cdn.crowdin.com/jipt/jipt.js';
-
-    if (!document.head) throw new Error('Unexpectedly missing <head>.');
-    document.head.appendChild(script);
     this.setState({ loading: false });
   }
 
@@ -63,7 +64,12 @@ class InContextTranslation extends React.Component<Props, State> {
         <CircularProgress />
       </Layout>
     ) : (
-      this.props.children
+      <>
+        <Head>
+          <script src="https://cdn.crowdin.com/jipt/jipt.js" />
+        </Head>
+        {this.props.children}
+      </>
     );
   }
 }
@@ -82,7 +88,6 @@ function initInContext(project: string, toLanguage: any) {
   window.localStorage.setItem(`jipt_language_id_${project}`, toLanguage.id);
   window.localStorage.setItem(`jipt_language_name_${project}`, toLanguage.name);
 
-  window._jipt = [];
   window._jipt.push(['preload_texts', true]);
   window._jipt.push(['project', project]);
 }
