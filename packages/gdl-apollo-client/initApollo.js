2feaf9c1c089ff6da6cb89bef8bf687120ffb920
Alexander Bjerkan <alb@knowit.no>
2018-12-05 13:55:24 +0100

    Clean up apollo initialization

--- packages/gdl-apollo-client/initApollo.js
+++ packages/gdl-apollo-client/initApollo.js
@@ -2,20 +2,13 @@
 import { ApolloClient, InMemoryCache } from 'apollo-boost';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
-
 import fetch from 'isomorphic-unfetch';
 
 let apolloClient = null;
 
-// Polyfill fetch() on the server (used by apollo-client)
-if (!process.browser) {
-  global.fetch = fetch;
-}
-
 function create(initialState, { getToken }) {
   const httpLink = createHttpLink({
-    uri: 'http://localhost:4000/graphql',
-    credentials: 'same-origin'
+    uri: 'http://localhost:4000/graphql'
   });
 
   const authLink = setContext((_, { headers }) => {
@@ -28,16 +21,19 @@ function create(initialState, { getToken }) {
     };
   });
 
-  // Check out https://github.com/zeit/next.js/pull/4611 if you want to use the AWSAppSyncClient
   return new ApolloClient({
     connectToDevTools: process.browser,
     ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
     link: authLink.concat(httpLink),
-    cache: new InMemoryCache().restore(initialState || {})
+    cache: new InMemoryCache().restore(initialState || {}),
+    fetch
   });
 }
 
-export default function initApollo(initialState, options) {
+export default function initApollo(
+  initialState: ?{},
+  options: { getToken: () => ?string }
+) {
   // Make sure to create a new client for every server-side request so that data
   // isn't shared between connections (which would be bad)
   if (!process.browser) {
