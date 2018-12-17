43fa467a12044440cfad536fdf2a0e39b0776eda
Alexander Bjerkan <alb@knowit.no>
2018-12-17 08:29:36 +0100

    a few more things

--- packages/gdl-frontend/components/Search/components/SearchInput.js
+++ packages/gdl-frontend/components/Search/components/SearchInput.js
@@ -13,7 +13,7 @@ import { Trans, I18n } from '@lingui/react';
 import { IconButton, Tooltip } from '@material-ui/core';
 import { withRouter } from 'next/router';
 import Router from 'next/router';
-import { css } from 'react-emotion';
+import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 
 import media from '../../../style/media';
@@ -88,11 +88,7 @@ const SearchInput = ({ autoFocus, className, onSubmit, onChange, value }) => (
             value={value}
           />
           <Tooltip title={<Trans>Search</Trans>}>
-            <IconButton
-              aria-label="Search"
-              className={styles.iconButton}
-              type="submit"
-            >
+            <IconButton aria-label="Search" css={iconButton} type="submit">
               <SearchIcon />
             </IconButton>
           </Tooltip>
@@ -143,13 +139,11 @@ const Input = styled('input')`
   }
 `;
 
-const styles = {
-  iconButton: css`
-    position: absolute;
-    left: 0;
-    width: 40px;
-    height: 40px;
-    margin-left: 5px;
-    color: #fff;
-  `
-};
+const iconButton = css`
+  position: absolute;
+  left: 0;
+  width: 40px;
+  height: 40px;
+  margin-left: 5px;
+  color: #fff;
+`;
--- packages/gdl-frontend/style/flex.js
+++ packages/gdl-frontend/style/flex.js
@@ -1,4 +1,4 @@
-import { css } from 'react-emotion';
+import { css } from '@emotion/core';
 
 const flexCenter = css`
   display: flex;
