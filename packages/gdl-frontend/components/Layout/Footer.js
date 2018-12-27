fb49a57e9fc5fa6fd0093b218f0cbff32d4fd210
Alexander Bjerkan <alb@knowit.no>
2018-12-27 12:16:41 +0100

    reafact: Simplify footer CSS

--- packages/gdl-frontend/components/Layout/Footer.js
+++ packages/gdl-frontend/components/Layout/Footer.js
@@ -23,90 +23,13 @@ const {
   publicRuntimeConfig: { zendeskUrl }
 }: ConfigShape = getConfig();
 
-const FooterStyle = styled('footer')`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 40px;
-  margin-bottom: 50px;
-  a {
-    text-decoration: none;
-    color: ${colors.text.default};
-    &:hover {
-      text-decoration: underline;
-    }
-  }
-`;
-
-const WrappingList = styled('ul')`
-  display: flex;
-  justify-content: space-between;
-  flex-grow: 3;
-  flex-wrap: wrap;
-  order: 2;
-  font-size: 0.8rem;
-  padding: 0;
-  margin-left: 28px;
-  list-style: none;
-  li {
-    padding: 8px;
-  }
-  ${media.tablet`
-    li {
-      width: 33%;
-     }
-  `} ${media.mobile`
-    li {
-      width: 50%;
-    }
-      margin-top: 20px;mh
-  `};
-`;
-
-const SocialMediaIcons = styled('div')`
-  display: flex;
-  justify-content: space-between;
-  flex-grow: 1;
-  margin: 14px 36px 20px 36px;
-  svg {
-    font-size: 2.25rem;
-  }
-  a {
-    color: ${colors.text.subtle};
-    &:hover {
-      color: ${colors.default};
-    }
-  }
-  ${media.tablet`
-    order: 4;
-  `};
-  ${media.mobile`
-    order: 1;
-  `};
-`;
-
-const CreativeCommons = styled('div')`
-  order: 3;
-  flex-grow: 1;
-  text-align: center;
-  a {
-    &:hover {
-    fill: ${colors.default};
-    }
-  ${media.mobile`
-    width: 100%;
-  `}
-`;
-
 const Footer = ({ online }) => {
   if (!online) return null;
   return (
     <Container
       size="large"
-      width="100%"
       // https://philipwalton.com/articles/normalizing-cross-browser-flexbox-bugs/
-      css={{ flexShrink: '0' }}
+      css={{ flexShrink: '0', width: '100%' }}
     >
       <FooterStyle>
         <div
@@ -115,13 +38,13 @@ const Footer = ({ online }) => {
               order: 2,
               width: '100%',
               borderBottom: `1px solid ${colors.base.grayLight}`,
-              margin: '10px'
+              margin: '10px 10px 18px'
             },
             media.tablet({ display: 'none' })
           ]}
         />
 
-        <WrappingList>
+        <LinkList>
           <li>
             <a href="https://home.digitallibrary.io/the-global-digital-library-uses-cookies/">
               <Trans>Cookie policy</Trans>
@@ -152,14 +75,10 @@ const Footer = ({ online }) => {
               <Trans>Blog</Trans>
             </a>
           </li>
-        </WrappingList>
+        </LinkList>
 
         <CreativeCommons>
-          <a
-            href="https://creativecommons.org/"
-            component="a"
-            aria-label="Creative Commons"
-          >
+          <a href="https://creativecommons.org/" aria-label="Creative Commons">
             <CCLogo css={{ width: '100px' }} />
           </a>
         </CreativeCommons>
@@ -191,5 +110,69 @@ const Footer = ({ online }) => {
     </Container>
   );
 };
+
+const FooterStyle = styled('footer')`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 40px;
+  margin-bottom: 50px;
+  a {
+    text-decoration: none;
+    color: ${colors.text.default};
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const LinkList = styled('ul')`
+  display: flex;
+  justify-content: space-between;
+  flex-grow: 3;
+  flex-wrap: wrap;
+  order: 2;
+  font-size: 0.8rem;
+  padding: 0;
+  margin-left: 28px;
+  list-style: none;
+  li {
+    padding: 8px;
+    width: 50%;
+    ${media.tablet`
+      width: 33%;
+  `}
+  }
+`;
+
+const SocialMediaIcons = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  flex-grow: 1;
+  margin: 14px 36px 20px 36px;
+  svg {
+    font-size: 2.25rem;
+  }
+  a {
+    color: ${colors.text.subtle};
+    &:hover {
+      color: ${colors.default};
+    }
+  }
+  order: 1;
+  ${media.tablet`
+    order: 4;
+  `};
+`;
+
+const CreativeCommons = styled('div')`
+  order: 3;
+  flex-grow: 1;
+  text-align: center;
+  a:hover {
+    fill: ${colors.default};
+  }
+`;
 
 export default withOnlineStatusContext(Footer);
