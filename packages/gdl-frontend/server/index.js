a4ebdd2a75c5be8b2d660f1f94f2dd7c9574effb
Kien Vu <kienv91@gmail.com>
2019-03-06 13:09:02 +0100

    added sentry release git hash

--- packages/gdl-frontend/server/index.js
+++ packages/gdl-frontend/server/index.js
@@ -48,6 +48,7 @@ app.prepare().then(() => {
   } else {
     if (REPORT_ERRORS) {
       Sentry.init({
+        release: '183bd659573525b65389779342c768dbf5ddb32d',
         dsn: `https://${SENTRY_PUBLIC_KEY}@sentry.io/${SENTRY_PROJECT_ID}`,
         environment: GDL_ENVIRONMENT
       });
