de2a495c42640b61cfc02d88d76ac72b84b2f8b3
Kien Vu <kienv91@gmail.com>
2019-04-03 14:15:26 +0200

    feat: set sentry tag to next-branch

--- packages/gdl-frontend/lib/initSentry.js
+++ packages/gdl-frontend/lib/initSentry.js
@@ -18,7 +18,7 @@ const {
 export default function() {
   if (process.env.NODE_ENV === 'production' && REPORT_ERRORS) {
     Sentry.init({
-      release: '183bd659573525b65389779342c768dbf5ddb32d',
+      release: 'next-branch',
       dsn: `https://${SENTRY_PUBLIC_KEY}@sentry.io/${SENTRY_PROJECT_ID}`,
       environment: GDL_ENVIRONMENT
     });
