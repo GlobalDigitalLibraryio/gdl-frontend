103e5f08d1f3256d1cc038fcc45ef3d3b15269e3
Frida Klockmann <fridaklockmann@gmail.com>
2019-07-17 13:00:50 +0200

    fixes

--- packages/gdl-frontend/components/WelcomeTutorial/DialogContent.js
+++ packages/gdl-frontend/components/WelcomeTutorial/DialogContent.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import DialogContent from '@material-ui/core/DialogContent';
 import { Card, CardContent } from '@material-ui/core';
-import Slide from '@material-ui/core/Slide';
 
 function getDialogContent(
   screenshotUrl: string,
@@ -19,9 +18,13 @@ function getDialogContent(
         height: '100%'
       }}
     >
-      <img src={screenshotUrl} style={{ width: '100%' }} />
+      <img
+        src={screenshotUrl}
+        style={{ width: '100%' }}
+        alt="screenshot from the page"
+      />
       <div style={{ display: 'flex', flexDirection: 'row', marginTop: 25 }}>
-        <img style={{ maxHeight: 182 }} src={graceImgUrl} />
+        <img style={{ maxHeight: 182 }} src={graceImgUrl} alt="Grace" />
         <Card style={{ marginLeft: '15px', width: '100%' }}>
           <CardContent>{message}</CardContent>
         </Card>
--- packages/gdl-frontend/components/WelcomeTutorial/WelcomeTutorial.js
+++ packages/gdl-frontend/components/WelcomeTutorial/WelcomeTutorial.js
@@ -3,8 +3,6 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import { Card, CardContent } from '@material-ui/core';
 import MobileStepper from '@material-ui/core/MobileStepper';
 import IconButton from '@material-ui/core/IconButton';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
