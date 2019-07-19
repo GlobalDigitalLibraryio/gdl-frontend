c4d318aef748580d5bdca8fa5b19511a214cfa87
Frida Klockmann <fridaklockmann@gmail.com>
2019-07-19 10:44:01 +0200

    swipe om mobile

--- packages/gdl-frontend/components/WelcomeTutorial/DialogContent.js
+++ packages/gdl-frontend/components/WelcomeTutorial/DialogContent.js
@@ -3,59 +3,68 @@ import React from 'react';
 import DialogContent from '@material-ui/core/DialogContent';
 import { Card, CardContent } from '@material-ui/core';
 
-function getDialogContent(
+type Props = {
   screenshotUrlM: string,
   screenshotUrl: string,
   graceImgUrl: string,
   message: string,
   fullscreen: boolean
-) {
-  let isFullscreenStyle;
-  let isFullscreenScreenshot;
-  if (fullscreen) {
-    isFullscreenStyle = { display: 'flex' };
-    isFullscreenScreenshot = screenshotUrlM;
-  } else {
-    isFullscreenStyle = { height: '270px' };
-    isFullscreenScreenshot = screenshotUrl;
-  }
-  return (
-    <DialogContent
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-around',
-        minHeight: 485,
-        height: '100%'
-      }}
-    >
-      <div style={isFullscreenStyle}>
-        <img
-          src={isFullscreenScreenshot}
-          alt="screenshot from the page"
-          style={{ width: '100%' }}
-        />
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'row', marginTop: 10 }}>
-        <img style={{ maxHeight: 182 }} src={graceImgUrl} alt="Grace" />
-        <Card style={{ marginLeft: '15px', width: '100%' }}>
-          <CardContent
-            style={{ display: 'table', height: '100%', width: '100%' }}
-          >
-            <p
-              style={{
-                textAlign: 'center',
-                verticalAlign: 'middle',
-                display: 'table-cell'
-              }}
+};
+
+class GetDialogContent extends React.Component<Props> {
+  render() {
+    let isFullscreenStyle;
+    let isFullscreenScreenshot;
+    if (this.props.fullscreen) {
+      isFullscreenStyle = { display: 'flex' };
+      isFullscreenScreenshot = this.props.screenshotUrlM;
+    } else {
+      isFullscreenStyle = { height: '270px' };
+      isFullscreenScreenshot = this.props.screenshotUrl;
+    }
+
+    return (
+      <DialogContent
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-around',
+          minHeight: 485,
+          height: '100%'
+        }}
+      >
+        <div style={isFullscreenStyle}>
+          <img
+            src={isFullscreenScreenshot}
+            alt="screenshot from the page"
+            style={{ width: '100%' }}
+          />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row', marginTop: 10 }}>
+          <img
+            style={{ maxHeight: 182 }}
+            src={this.props.graceImgUrl}
+            alt="Grace"
+          />
+          <Card style={{ marginLeft: '15px', width: '100%' }}>
+            <CardContent
+              style={{ display: 'table', height: '100%', width: '100%' }}
             >
-              {message}
-            </p>
-          </CardContent>
-        </Card>
-      </div>
-    </DialogContent>
-  );
+              <p
+                style={{
+                  textAlign: 'center',
+                  verticalAlign: 'middle',
+                  display: 'table-cell'
+                }}
+              >
+                {this.props.message}
+              </p>
+            </CardContent>
+          </Card>
+        </div>
+      </DialogContent>
+    );
+  }
 }
 
-export default getDialogContent;
+export default GetDialogContent;
--- packages/gdl-frontend/components/WelcomeTutorial/WelcomeTutorial.js
+++ packages/gdl-frontend/components/WelcomeTutorial/WelcomeTutorial.js
@@ -7,7 +7,7 @@ import MobileStepper from '@material-ui/core/MobileStepper';
 import Slide from '@material-ui/core/Slide';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
-import getDialogContent from './DialogContent';
+import GetDialogContent from './DialogContent';
 
 const grace1 = '/static/img/grace1.png';
 const grace2 = '/static/img/grace2.png';
@@ -18,18 +18,39 @@ const menu = '/static/img/menu.png';
 const saveOfflineM = '/static/img/saveOfflineM.png';
 const menuM = '/static/img/menuM.png';
 
+const fullscreenWidth = 768;
+
 type State = {
   open: boolean,
   width: number,
-  height: number,
   activeStep: number,
   direction: 'left' | 'right',
   isHidden1: string,
   isHidden2: string,
-  isHidden3: string
+  isHidden3: string,
+  left: number,
+  originalOffset: number,
+  velocity: number,
+  timeOfLastDragEvent: number,
+  touchStartX: number,
+  prevTouchX: number,
+  beingTouched: boolean,
+  height: number,
+  intervalId: string | null
 };
 type Props = {
   shouldOpen: boolean
+};
+type touchMoveEventType = {
+  targetTouches: {
+    clientX: number
+  }
+};
+type TargetTouchesArray = {
+  clientX: number
+};
+type touchStartEventType = {
+  targetTouches: Array<TargetTouchesArray>
 };
 
 const Styles = {
@@ -67,39 +88,53 @@ class WelcomeTutorial extends React.Component<Props, State> {
       direction: 'left',
       isHidden1: 'flex',
       isHidden2: 'none',
-      isHidden3: 'none'
+      isHidden3: 'none',
+      left: 0,
+      originalOffset: 0,
+      velocity: 0,
+      timeOfLastDragEvent: 0,
+      touchStartX: 0,
+      prevTouchX: 0,
+      beingTouched: false,
+      intervalId: null
     };
   }
 
-  componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions.bind(this));
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener(
-      'resize',
-      this.updateWindowDimensions.bind(this)
-    );
-  }
-
-  updateWindowDimensions() {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
-  }
-
-  handleClickOpen() {
+  handleTouchStart(touchStartEvent: touchStartEventType, mobile: boolean) {
+    if (this.state.intervalId !== null && mobile) {
+      window.clearInterval(this.state.intervalId);
+    }
     this.setState({
-      open: true,
-      activeStep: 0
+      originalOffset: this.state.left,
+      velocity: 0,
+      timeOfLastDragEvent: Date.now(),
+      touchStartX: touchStartEvent.targetTouches[0].clientX,
+      beingTouched: true,
+      intervalId: null
     });
   }
 
+  animateSlidingToZero() {
+    let { left, velocity, beingTouched } = this.state;
+    if (!beingTouched && left < -0.01) {
+      velocity += 10 * 0.033;
+      left += velocity;
+      if (left < -350) {
+        window.clearInterval(this.state.intervalId);
+      }
+      this.setState({ left, velocity });
+    } else if (!beingTouched) {
+      left = 0;
+      velocity = 0;
+      window.clearInterval(this.state.intervalId);
+      this.setState({ left, velocity, intervalId: null, originalOffset: 0 });
+    }
+  }
   handleClose() {
     this.setState({
       open: false,
       isHidden1: 'flex',
-      isHidden2: 'none',
-      isHidden3: 'none'
+      isHidden2: 'none'
     });
   }
 
@@ -127,6 +162,62 @@ class WelcomeTutorial extends React.Component<Props, State> {
       }
     }
   }
+  handleMove(clientX: number) {
+    if (this.state.beingTouched) {
+      const touchX = clientX;
+      const currTime = Date.now();
+      const elapsed = currTime - this.state.timeOfLastDragEvent;
+      const velocity = (20 * (touchX - this.state.prevTouchX)) / elapsed;
+      let deltaX = touchX - this.state.touchStartX + this.state.originalOffset;
+      if (deltaX < -350) {
+        this.handleNext();
+        deltaX = 0;
+        this.setState({ beingTouched: false });
+      } else if (deltaX > 350) {
+        if (this.state.activeStep > 0) {
+          this.handleBack();
+          deltaX = 0;
+          this.setState({ beingTouched: false });
+        }
+      }
+      this.setState({
+        left: deltaX,
+        velocity,
+        timeOfLastDragEvent: currTime,
+        prevTouchX: touchX
+      });
+    }
+  }
+  handleTouchEnd(mobile: boolean) {
+    if (mobile) {
+      this.setState({
+        velocity: this.state.velocity,
+        touchStartX: 0,
+        beingTouched: false,
+        intervalId: window.setInterval(this.animateSlidingToZero.bind(this), 33)
+      });
+    }
+  }
+
+  handleTouchMove(touchMoveEvent: touchMoveEventType, mobile: boolean) {
+    if (mobile) this.handleMove(touchMoveEvent.targetTouches[0].clientX);
+  }
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions.bind(this));
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
+
+  handleClickOpen() {
+    this.setState({
+      open: true,
+      activeStep: 0
+    });
+  }
+
   handleBack() {
     this.setState({
       direction: 'right',
@@ -149,27 +240,46 @@ class WelcomeTutorial extends React.Component<Props, State> {
   }
 
   render() {
-    let mobile: boolean = this.state.width < 500;
+    let mobile: boolean = this.state.width < fullscreenWidth;
     return (
       <Dialog
         fullScreen={mobile}
         open={this.state.open}
         onClose={this.handleClose.bind(this)}
       >
-        <div style={Styles.dialogWindow}>
+        <div
+          style={Styles.dialogWindow}
+          onTouchStart={touchStartEvent =>
+            this.handleTouchStart(touchStartEvent, mobile)
+          }
+          onTouchMove={touchMoveEvent =>
+            this.handleTouchMove(touchMoveEvent, mobile)
+          }
+          onTouchEnd={() => {
+            this.handleTouchEnd(mobile);
+          }}
+        >
           <Slide
             style={{ height: '100%' }}
             direction={this.state.direction}
             in={this.state.activeStep === 0}
           >
-            <div style={{ display: this.state.isHidden1 }}>
-              {getDialogContent(
-                selectLanguage,
-                selectLanguage,
-                grace1,
-                `Hi! If you want to change the language, click on the globe at the top-right corner of the screen!`,
-                mobile
-              )}
+            <div
+              style={{
+                display: this.state.isHidden1,
+                left: this.state.left + 'px',
+                position: 'relative'
+              }}
+            >
+              <div>
+                <GetDialogContent
+                  screenshotUrlM={selectLanguage}
+                  screenshotUrl={selectLanguage}
+                  graceImgUrl={grace1}
+                  message="Hi! If you want to change the language, click on the globe at the top-right corner of the screen!"
+                  fullscreen={mobile}
+                />
+              </div>
             </div>
           </Slide>
           <Slide
@@ -177,14 +287,20 @@ class WelcomeTutorial extends React.Component<Props, State> {
             direction={this.state.direction}
             in={this.state.activeStep === 1}
           >
-            <div style={{ display: this.state.isHidden2 }}>
-              {getDialogContent(
-                saveOfflineM,
-                saveOffline,
-                grace2,
-                'You can save books for later and read them offline by clicking the Save offline icon.',
-                mobile
-              )}
+            <div
+              style={{
+                display: this.state.isHidden2,
+                left: this.state.left + 'px',
+                position: 'relative'
+              }}
+            >
+              <GetDialogContent
+                screenshotUrlM={saveOfflineM}
+                screenshotUrl={saveOffline}
+                graceImgUrl={grace2}
+                message="You can save books for later and read them offline by clicking the Save offline icon!!."
+                fullscreen={mobile}
+              />
             </div>
           </Slide>
           <Slide
@@ -192,14 +308,20 @@ class WelcomeTutorial extends React.Component<Props, State> {
             direction={this.state.direction}
             in={this.state.activeStep === 2}
           >
-            <div style={{ display: this.state.isHidden3 }}>
-              {getDialogContent(
-                menuM,
-                menu,
-                grace3,
-                'The menu is in the top-left corner. From the menu you can access Categories, Favorites and more',
-                mobile
-              )}
+            <div
+              style={{
+                display: this.state.isHidden3,
+                left: this.state.left + 'px',
+                position: 'relative'
+              }}
+            >
+              <GetDialogContent
+                screenshotUrlM={menuM}
+                screenshotUrl={menu}
+                graceImgUrl={grace3}
+                message="The menu is in the top-left corner. From the menu you can access Categories, Favorites and more."
+                fullscreen={mobile}
+              />
             </div>
           </Slide>
           <div>
