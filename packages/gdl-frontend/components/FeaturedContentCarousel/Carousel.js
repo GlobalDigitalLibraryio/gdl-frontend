289125a99e6c5fbf0ed65f16b32aee13130800c3
Frida Klockmann <fridaklockmann@gmail.com>
2019-08-01 15:00:05 +0200

    circkle

--- packages/gdl-frontend/components/FeaturedContentCarousel/Carousel.js
+++ packages/gdl-frontend/components/FeaturedContentCarousel/Carousel.js
@@ -4,7 +4,7 @@ import { Button, CardContent, Typography } from '@material-ui/core';
 import { View, Hidden } from '../../elements';
 import Pagination from '../FeaturedContentCarousel/Pagination';
 import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
+import { autoPlay, virtualize } from 'react-swipeable-views-utils';
 import { KeyboardArrowRight, KeyboardArrowLeft } from '@material-ui/icons/';
 import Head from '../../components/Head';
 import {
@@ -20,7 +20,7 @@ import type { HomeContent_featuredContent as FeaturedContent } from '../../gqlTy
 import { colors } from '../../style/theme';
 
 type State = { index: number };
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+const AutoPlaySwipeableViews = autoPlay(virtualize(SwipeableViews));
 
 type Props = {|
   featuredContent: Array<FeaturedContent>
@@ -39,81 +39,97 @@ class Carousel extends React.Component<Props, State> {
       index: this.state.index - 1
     });
   };
-
-  goToLastPage = () => {
-    this.setState({
-      index: this.props.featuredContent.length - 1
-    });
+  cardContent = (content: FeaturedContent) => {
+    return (
+      // Specifying width here makes text in IE11 wrap
+      <View alignItems="center" style={{ width: '100%' }}>
+        <Typography
+          lang={content.language.code}
+          align="center"
+          variant="h5"
+          component="h2"
+          gutterBottom
+          // Specifying width here makes text in IE11 wrap
+          style={{ width: '100%' }}
+        >
+          {content.title}
+        </Typography>
+        <Typography
+          lang={content.language.code}
+          align="center"
+          paragraph
+          // Specifying width here makes text in IE11 wrap
+          style={{ width: '100%' }}
+        >
+          {content.description}
+        </Typography>
+        <Button
+          onClick={() => logEvent('Navigation', 'Featured', content.title)}
+          href={content.link}
+          variant="contained"
+          color="primary"
+          size="large"
+        >
+          <FormattedMessage id="More" defaultMessage="More" />
+        </Button>
+      </View>
+    );
   };
-  goToFirstPage = () => {
-    this.setState({
-      index: 0
-    });
+  card = (content: FeaturedContent) => {
+    return (
+      <>
+        <Head image={content.imageUrl} />
+        <Banner src={content.imageUrl}>
+          <HeroCovertitle>
+            <Typography
+              component="h1"
+              variant="h6"
+              css={{ color: colors.base.white }}
+            >
+              <FormattedMessage id="Featured" defaultMessage="Featured" />
+            </Typography>
+          </HeroCovertitle>
+          <HeroCardTablet>
+            {/* Specifying width here makes text in IE11 wrap*/}
+            <CardContent style={{ width: '100%' }}>
+              {this.cardContent(content)}
+            </CardContent>
+          </HeroCardTablet>
+        </Banner>
+      </>
+    );
+  };
+  slideRenderer = (params: { index: number, key: number }) => {
+    const { key } = params;
+    const { featuredContent } = this.props;
+    const indexPage = Math.abs(this.state.index % featuredContent.length);
+    const content = featuredContent[indexPage];
+
+    return (
+      <div key={key}>
+        {this.card(content)}
+
+        <HeroCardMobile>
+          <CardContent>
+            {this.cardContent(content)}
+            <Hidden only="mobile">
+              <div style={{ paddingTop: '8px' }}>
+                <Pagination
+                  dots={featuredContent.length}
+                  index={indexPage}
+                  onChangeIndex={index => this.setState({ index })}
+                />
+              </div>
+            </Hidden>{' '}
+          </CardContent>
+        </HeroCardMobile>
+      </div>
+    );
   };
   render() {
     const { index } = this.state;
     const { featuredContent } = this.props;
-    const cardContent = content => {
-      return (
-        // Specifying width here makes text in IE11 wrap
-        <View alignItems="center" style={{ width: '100%' }}>
-          <Typography
-            lang={content.language.code}
-            align="center"
-            variant="h5"
-            component="h2"
-            gutterBottom
-            // Specifying width here makes text in IE11 wrap
-            style={{ width: '100%' }}
-          >
-            {content.title}
-          </Typography>
-          <Typography
-            lang={content.language.code}
-            align="center"
-            paragraph
-            // Specifying width here makes text in IE11 wrap
-            style={{ width: '100%' }}
-          >
-            {content.description}
-          </Typography>
-          <Button
-            onClick={() => logEvent('Navigation', 'Featured', content.title)}
-            href={content.link}
-            variant="contained"
-            color="primary"
-            size="large"
-          >
-            <FormattedMessage id="More" defaultMessage="More" />
-          </Button>
-        </View>
-      );
-    };
 
-    const card = content => {
-      return (
-        <>
-          <Head image={content.imageUrl} />
-          <Banner src={content.imageUrl}>
-            <HeroCovertitle>
-              <Typography
-                component="h1"
-                variant="h6"
-                css={{ color: colors.base.white }}
-              >
-                <FormattedMessage id="Featured" defaultMessage="Featured" />
-              </Typography>
-            </HeroCovertitle>
-            <HeroCardTablet>
-              {/* Specifying width here makes text in IE11 wrap*/}
-              <CardContent style={{ width: '100%' }}>
-                {cardContent(content)}
-              </CardContent>
-            </HeroCardTablet>
-          </Banner>
-        </>
-      );
-    };
     return (
       <div style={{ position: 'relative' }}>
         {featuredContent.length !== 1 ? (
@@ -122,73 +138,30 @@ class Carousel extends React.Component<Props, State> {
               interval={7000}
               index={index}
               onChangeIndex={index => this.setState({ index })}
-            >
-              {featuredContent.map(content => (
-                <div key={content.id}>
-                  {card(content)}
-
-                  <HeroCardMobile>
-                    <CardContent>
-                      {cardContent(content)}
-                      <Hidden only="mobile">
-                        <div style={{ paddingTop: '8px' }}>
-                          <Pagination
-                            dots={featuredContent.length}
-                            index={index}
-                            onChangeIndex={index => this.setState({ index })}
-                          />
-                        </div>
-                      </Hidden>{' '}
-                    </CardContent>
-                  </HeroCardMobile>
-                </div>
-              ))}
-            </AutoPlaySwipeableViews>
+              slideRenderer={this.slideRenderer}
+            />
             <Hidden only="desktop">
-              {(index !== 0 && (
-                <div
-                  css={arrowLeftContainer}
-                  aria-label="Previous"
-                  onClick={this.handlePrevIndex}
-                >
-                  {' '}
-                  <KeyboardArrowLeft style={{ color: 'white' }} />
-                </div>
-              )) ||
-                (index === 0 && (
-                  <div
-                    css={arrowLeftContainer}
-                    aria-label="Previous"
-                    onClick={this.goToLastPage}
-                  >
-                    {' '}
-                    <KeyboardArrowLeft style={{ color: 'white' }} />
-                  </div>
-                ))}
-              {(index !== featuredContent.length - 1 && (
-                <div
-                  css={arrowRightContainer}
-                  aria-label="Next"
-                  onClick={this.handleNextIndex}
-                >
-                  {' '}
-                  <KeyboardArrowRight style={{ color: 'white' }} />
-                </div>
-              )) ||
-                (index === featuredContent.length - 1 && (
-                  <div
-                    css={arrowRightContainer}
-                    aria-label="Next"
-                    onClick={this.goToFirstPage}
-                  >
-                    {' '}
-                    <KeyboardArrowRight style={{ color: 'white' }} />
-                  </div>
-                ))}
+              <div
+                css={arrowLeftContainer}
+                aria-label="Previous"
+                onClick={this.handlePrevIndex}
+              >
+                {' '}
+                <KeyboardArrowLeft style={{ color: 'white' }} />
+              </div>
+
+              <div
+                css={arrowRightContainer}
+                aria-label="Next"
+                onClick={this.handleNextIndex}
+              >
+                {' '}
+                <KeyboardArrowRight style={{ color: 'white' }} />
+              </div>
               <div css={dotsContainer}>
                 <Pagination
                   dots={featuredContent.length}
-                  index={index}
+                  index={Math.abs(index % featuredContent.length)}
                   onChangeIndex={index => this.setState({ index })}
                 />
               </div>
@@ -197,7 +170,7 @@ class Carousel extends React.Component<Props, State> {
               <div css={dotsContainer}>
                 <Pagination
                   dots={featuredContent.length}
-                  index={index}
+                  index={Math.abs(index % featuredContent.length)}
                   onChangeIndex={index => this.setState({ index })}
                 />
               </div>
@@ -205,10 +178,10 @@ class Carousel extends React.Component<Props, State> {
           </>
         ) : (
           <>
-            {card(featuredContent[0])}
+            {this.card(featuredContent[0])}
 
             <HeroCardMobile>
-              <CardContent>{cardContent(featuredContent[0])}</CardContent>
+              <CardContent>{this.cardContent(featuredContent[0])}</CardContent>
             </HeroCardMobile>
           </>
         )}
