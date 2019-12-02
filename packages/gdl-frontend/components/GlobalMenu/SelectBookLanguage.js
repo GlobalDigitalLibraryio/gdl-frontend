3dc64d4b8dddc553f2420d67c9e1641d678acd1f
Kien Vu <kienv91@gmail.com>
2019-12-02 15:10:28 +0100

    #643: redirect correctly on changing lang

--- packages/gdl-frontend/components/GlobalMenu/SelectBookLanguage.js
+++ packages/gdl-frontend/components/GlobalMenu/SelectBookLanguage.js
@@ -16,9 +16,9 @@ import LanguageList from '../LanguageList';
 import { getBookLanguageCode } from '../../lib/storage';
 import { GdlI18nConsumer } from '../GdlI18nProvider';
 
-function linkProps(language, category) {
+function linkProps(language, route) {
   return {
-    route: category,
+    route: route === '/' ? 'books' : route.replace('/', ''),
     params: { lang: language.code }
   };
 }
